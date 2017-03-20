1e5afedff0db38616b7160b1d786bac58b1ac391
Josef Raska <josef.raska@ocado.com>
2017-03-20 18:45:25 +0000

    Initial commit

--- rx-codelab/src/main/java/com/jraska/rx/codelab/http/HttpBinApi.java
+++
@@ -1,9 +0,0 @@
-package com.jraska.rx.codelab.http;
-
-import io.reactivex.Observable;
-import retrofit2.http.GET;
-
-public interface HttpBinApi {
-    @GET("/get")
-    Observable<RequestInfo> getRequest();
-}
