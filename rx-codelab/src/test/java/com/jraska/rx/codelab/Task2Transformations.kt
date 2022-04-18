9dfa4d6d7228b3e6e567124922a0efd9c87f13e9
Aleksandra <portos100.00.1a@gmail.com>
2022-04-18 16:18:34 +0300

    Solution to task 2

--- rx-codelab/src/test/java/com/jraska/rx/codelab/Task2Transformations.kt
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task2Transformations.kt
@@ -1,6 +1,8 @@
 package com.jraska.rx.codelab
 
+import com.jraska.rx.codelab.http.GitHubConverter
 import com.jraska.rx.codelab.http.HttpModule
+import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
 
@@ -15,16 +17,24 @@ class Task2Transformations {
   @Test
   fun map_convertUserDto() {
     // TODO: Use gitHubApi to get and print string representation of user with `LOGIN`. Use `User` and `GitHubConverter`
+    gitHubApi.getUser(LOGIN).map { GitHubConverter.convert(it) }.subscribe { println(it) }
   }
 
   @Test
   fun flatMap_getFirstUserDetailAfterGettingList() {
     // TODO:  Use gitHubApi to first get list of users and subsequently get its first user by other request
+    gitHubApi.getFirstUsers().flatMap { Observable.fromIterable(it) }
+      .filter { it.id == 1 }
+      .map { GitHubConverter.convert(it) }
+      .subscribe { println(it) }
   }
 
   @Test
   fun replayAutoConnect_oneRequestForTwoSubscriptions() {
     // TODO: Get again the user with `LOGIN`, but subscribe twice and print only with 1 network request
+    val observable = gitHubApi.getUser(LOGIN).map { GitHubConverter.convert(it) }.replay().autoConnect()
+    observable.subscribe { println(it) }
+    observable.subscribe { println(it) }
   }
 
   companion object {
