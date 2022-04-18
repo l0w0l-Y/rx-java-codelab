fe7728c2292bc411fa45e95a6a07dfce8cd67389
Aleksandra <portos100.00.1a@gmail.com>
2022-04-18 17:09:25 +0300

    Solution to task 4

--- rx-codelab/src/test/java/com/jraska/rx/codelab/Task4ErrorHandling.kt
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task4ErrorHandling.kt
@@ -17,21 +17,25 @@ class Task4ErrorHandling {
   @Test
   fun printErrorMessage() {
     // TODO: Subscribe and incoming error message - httpBinApi.failingGet(), subscribe() with 2 parameters
+    httpBinApi.failingGet().subscribe(System.out::println, System.out::println)
   }
 
   @Test
   fun onErrorReturnItem_emitCustomItemOnError() {
     // TODO: When an error happens, emit syntheticBody(), httpBinApi.failingGet()
+    httpBinApi.failingGet().onErrorReturnItem(syntheticBody()).map { it.string() }.subscribe(System.out::println)
   }
 
   @Test
   fun onErrorResumeNext_subscribeToExtraObservableOnError() {
     // TODO: When an error happens, subscribe to extra observable - httpBinApi.backupGet()
+    httpBinApi.failingGet().onErrorResumeNext(httpBinApi.backupGet()).map { it.string() }.subscribe(System.out::println)
   }
 
   @Test
   fun retry_retryOnError() {
     // TODO: httpBinApi.flakyGet is a bit flakey and often fails, use retry to make it always complete
+    httpBinApi.flakyGet().retry().subscribe(System.out::println)
   }
 
   companion object {
