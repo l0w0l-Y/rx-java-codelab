5b2efee478aa176918f6093b68d119f131e58119
Aleksandra <portos100.00.1a@gmail.com>
2022-04-19 12:00:34 +0300

    Solution to task 9

--- rx-codelab/src/test/java/com/jraska/rx/codelab/Task9WhereWeCanFindRxJavaHandy.kt
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task9WhereWeCanFindRxJavaHandy.kt
@@ -6,6 +6,7 @@ import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
 import org.junit.After
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 class Task9WhereWeCanFindRxJavaHandy {
   private val httpBinApi = HttpModule.httpBinApi()
@@ -16,6 +17,9 @@ class Task9WhereWeCanFindRxJavaHandy {
 
     val request = httpBinApi.getRequest()
       .subscribeOn(Schedulers.io())
+      .repeatWhen { refreshSignal }
+      .share()
+      .cache()
 
     // TODO: Perform only one request for both following subscribes - share()
     request.subscribe()
@@ -34,7 +38,9 @@ class Task9WhereWeCanFindRxJavaHandy {
   fun repeat_pollingNetwork() {
     val request = httpBinApi.getRequest()
     val endOfPolling = System.currentTimeMillis() + 1000
-
+    request.repeatWhen { observable -> observable.delay(100, TimeUnit.MILLISECONDS) }
+      .takeUntil { System.currentTimeMillis() > endOfPolling }
+      .subscribe()
     // TODO: Implement polling on the request for one second - repeat(), takeUntil()
     // TODO: Add 100ms delay between requests - repeatWhen(), delay()
   }
@@ -46,6 +52,8 @@ class Task9WhereWeCanFindRxJavaHandy {
       .share()
 
     val requestWithCache = RequestInfoCache.requestInfo.mergeWith(request)
+    val observableWithCache = requestWithCache.ambWith(requestWithCache)
+    observableWithCache.subscribe(System.out::println)
 
     // TODO: requestWithCache has a bug! In rare case when the network request happens to be faster than the cache, ...
     // TODO: ...the last emission will be cached value. Fix this with ambWith
